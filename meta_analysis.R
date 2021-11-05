fda5a0cf7767b5d76ee69e06ab4886691f01edf1
Ibukun <55224724+ibukunlola@users.noreply.github.com>
2021-11-05 16:06:38 +0000

    Update meta_analysis.R

--- meta_analysis.R
+++ meta_analysis.R
@@ -1,6 +1,8 @@
 install.packages("meta")
 library(meta)
 
+col.contour = c("gray75", "gray85", "gray95")
+
 # Load the data from github.
 data <- read.csv(url("https://raw.githubusercontent.com/ibukunlola/il181-metaanalysis/main/metaanalysis_data.csv"))
 head(data)
@@ -23,15 +25,15 @@ m.gen.random <- metagen(TE = log(Effect.size),
                  fixed = F,
                  random = TRUE,
                  hakn = TRUE,
-                 title = "Effect of masks on COVID contraction rate (odds ratio)")
+                 title = "Random Effects Model - Effect of masks on COVID contraction rate (odds ratio)")
 summary(m.gen.random)
 
 # Create the forest plot
 forest.random <- forest.meta(m.gen.random, 
             sortvar = Effect.size,
             comb.random = TRUE,
-            comb.fixed = T,
-            print.tau2 = F,
+            comb.fixed = F,
+            print.tau2 = T,
             print.I2 = FALSE,
             leftcols = c("studlab"),
             rightcols = c("effect.ci"),
@@ -39,10 +41,18 @@ forest.random <- forest.meta(m.gen.random,
             rightlabs = c("OR [95% CI]"),
             leftlabs = c("Authors"))
 
+
 # Create the funnel plot - scatterplot of the studies with std. err on the y-axis and OR on the x-axis
 funnel.random <- funnel.meta(m.gen.random,
-            xlim = c(0.04, 2),
-            studlab = TRUE)
+                            xlim = c(0.04, 2),
+                            studlab = TRUE,
+                            contour = c(0.9, 0.95, 0.99),
+                            col.contour = col.contour)
+legend(x = 1.2, y = 0, 
+       legend = c("p < 0.1", "p < 0.05", "p < 0.01"),
+       fill = col.contour)
+
+title("Contour-Enhanced Funnel Plot (Odds Ratios)")
 
 
 # FIXED EFFECTS MODEL
@@ -75,4 +85,11 @@ forest.fixed <- forest.meta(m.gen.fixed,
 # Create the funnel plot - scatterplot of the studies with std. err on the y-axis and OR on the x-axis
 funnel.fixed <- funnel.meta(m.gen.fixed,
                              xlim = c(0.04, 2),
-                             studlab = TRUE)
+                             studlab = TRUE,
+                            contour = c(0.9, 0.95, 0.99),
+                            col.contour = col.contour)
+legend(x = 1.2, y = 0, 
+       legend = c("p < 0.1", "p < 0.05", "p < 0.01"),
+       fill = col.contour)
+
+title("Contour-Enhanced Funnel Plot (Odds Ratios)")
